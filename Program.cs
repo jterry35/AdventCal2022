427c1f4780b1e73a0b9a31aefe49c88615d443ad
Jason Terry <jterry@pinnaclesolutionsinc.com>
2022-12-03 09:12:24 -0600

    Day 3 - Part1

--- Program.cs
+++ Program.cs
@@ -1,109 +1,50 @@
-﻿const string ROCK = "A";
-const string PAPER = "B";
-const string SCISSORS = "C";
+﻿string[] lines = System.IO.File.ReadAllLines("input.txt");
 
-Dictionary<string, MyHand> handDict = new Dictionary<string, MyHand>();
-handDict.Add(ROCK, new MyHand() { Me = ROCK, MyPoints = 1, ThingIBeat = SCISSORS, ThingILose = PAPER });
-handDict.Add(PAPER, new MyHand() { Me = PAPER,  MyPoints = 2, ThingIBeat = ROCK, ThingILose = SCISSORS });
-handDict.Add(SCISSORS, new MyHand() { Me = SCISSORS, MyPoints = 3, ThingIBeat = PAPER, ThingILose = ROCK });
+int runningScore = 0;
 
-string[] lines = System.IO.File.ReadAllLines("input.txt");
-int totalPts = 0;
-
-foreach(var line in lines)
+foreach (var line in lines)
 {
-    string[] parts = line.Split(' ');
-    string myPlay = "";
-    string oppPlay = parts[0];
+    // split line in half
+    // split each char into an array
+    // compare the two arrays
+    // score chars based on ascii
 
-    switch (parts[1])
+    string secondHalf = line.Substring(line.Length / 2);
+    string firstHalf = line.Replace(secondHalf, "");
+
+    char[] firstChars = firstHalf.ToCharArray();
+    char[] secChars = secondHalf.ToCharArray();
+
+    char[] dups = firstChars.Where(x => secChars.Contains(x)).Distinct().ToArray();
+    
+    foreach(char d in dups)
     {
-        case "X": // Lose
-            Console.Write("Should lose ---");
-            myPlay = GetHandThatWillLose(oppPlay);
-            break;
-        case "Y": // draw
-            Console.Write("Should tie ---");
-            myPlay = oppPlay;
-            break;
-        case "Z": // win
-            Console.Write("Should win ---");
-            myPlay = GetHandThatWillWin(oppPlay);
-            break;
-        default:
-            break;
-    }
-
-    handDict[myPlay].DetermineWinner(ref totalPts, oppPlay);
-}
-
-string GetHandThatWillLose(string oppPlay)
-{
-    if (oppPlay == ROCK)
-        return SCISSORS;
-    else if (oppPlay == PAPER)
-        return ROCK;
-    else
-        return PAPER;
-}
-
-string GetHandThatWillWin(string oppPlay)
-{
-    if (oppPlay == ROCK)
-        return PAPER;
-    else if (oppPlay == PAPER)
-        return SCISSORS;
-    else
-        return ROCK;
-}
-
-
-
-Console.WriteLine("Score " + totalPts);
-
-
-
-
-
-
-
-
-public class MyHand
-{
-    public string Me { get; set; }
-    public int MyPoints { get; set; }
-    public string ThingIBeat { get; set; }
-    public string ThingILose { get; set; }
-
-    internal void DetermineWinner(ref int totalPts, string oppPlay)
-    {
-        int score = 0;
-        string outcome = "";
-
-        if(oppPlay == ThingIBeat)
-        {
-            // i won
-            score = MyPoints + 6;
-            outcome = $"Win - {Me} Beats {oppPlay} = {score}";
-        }
-        else if(oppPlay == ThingILose)
-        {
-            // i lost
-            score = MyPoints + 0;
-            outcome = $"Lose {oppPlay} Beats {Me} = {score}";
-        }
-        else
-        {
-            // tie
-            score = MyPoints + 3;
-            outcome = $"Tie {Me} Ties w/ {oppPlay} = {score}";
-        }
-
-        Console.WriteLine(outcome);
-
-        totalPts = totalPts + score; 
+        runningScore += GetScore(d);
     }
 }
 
+Console.WriteLine("Score = " + runningScore);
 
 
+
+int GetScore(char dup)
+{
+    int ascii = (int)dup;
+    int subtractor = 0;
+    int score = 0;
+
+    if (ascii < 97) // capital
+    {
+        subtractor = 64;
+        score = dup - subtractor + 26;
+    }
+    else
+    {
+        subtractor = 96;
+        score = dup - subtractor;
+    }
+
+    
+
+    return score;
+}
